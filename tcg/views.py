49618d501a7068a9b3810d5d7598bfb7e8a9723e
roy4ik <67188481+roy4ik@users.noreply.github.com>
2020-10-19 11:20:14 +0300

    added home view, url and fixed base

--- tcg/views.py
+++ tcg/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    context = {
+        'Title': 'PokeTrade',
+    }
+    return render(request, 'base/base.html', context=context)
+
