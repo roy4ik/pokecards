a95a97395559cd17f4ace80f76b274437134b5d2
roy4ik <67188481+roy4ik@users.noreply.github.com>
2020-10-20 16:22:47 +0300

    created all cards view

--- tcg/views.py
+++ tcg/views.py
@@ -7,12 +7,25 @@ import json
 import requests
 # Create your views here.
 def all_cards(request):
+    context = {}
     try:
-        all_pokemon = requests.get("https://pokeapi.co/api/v2/pokemon?limit=100&offset=200").json[2]
-        context = {}
+        all_pokemon = requests.get("https://pokeapi.co/api/v2/pokemon?limit=1050").json()['results']
+        for pokemon in range(1,len(all_pokemon)-1):
+            img_url = f'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/{pokemon}.png'
+            # print(img_url)
+            all_pokemon[pokemon-1].update({'img' : img_url})
+            # pokeapi_url = f'https://pokeapi.co/api/v2/pokemon/{pokemon}'
+            # print(requests.get(pokeapi_url))
+            # print(f'Updating {pokeapi_url} at {all_pokemon[pokemon-1]}')
+            # all_pokemon[pokemon-1].update({'weight' : requests.get(pokeapi_url).json()['weight'],
+            # 'img' : requests.get(pokeapi_url).json()['sprites']['front_default'],
+            # 'type' : requests.get(pokeapi_url).json()['types'][0]['type']['name'],
+            # 'species' : requests.get(pokeapi_url).json()['species']['name'],
+            # 'color' : requests.get(requests.get(pokeapi_url).json()['species']['url']).json()['color']['name']})
+            # print(f"Pokemon List {'{:.2f}'.format((pokemon/len(all_pokemon)*100))}% updated")
         context.update({'all_pokemon': all_pokemon})
-        print(context['all_pokemon'])
     except ConnectionError:
         error_msg = "Oops couldn't connect to the API"
+        context.update({'all_pokemon': error_msg})
     finally: 
         return render (request, 'all_cards.html', context)
