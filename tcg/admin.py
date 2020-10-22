ec0815beb6c68befb7c9cf3fe3437f9bd40b7fa1
roy4ik <67188481+roy4ik@users.noreply.github.com>
2020-10-22 20:53:59 +0300

    trade details working with create offer button

--- tcg/admin.py
+++ tcg/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-admin.site.register(models.)
+admin.site.register(Vault)
+admin.site.register(Trade)
+admin.site.register(Counter_Offer)
+admin.site.register(Pokemon)
+admin.site.register(Species)
+admin.site.register(Type)
+admin.site.register(Color)
