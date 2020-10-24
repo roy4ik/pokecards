916a369ab3c9ca94186146e855629fd49f84291f
roy4ik <67188481+roy4ik@users.noreply.github.com>
2020-10-24 18:59:44 +0300

    removed signup complete from views, urls,changed success

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,5 +10,4 @@ urlpatterns = [
     path('logout', LogoutView.as_view(template_name='base/base.html'), name ='logout'),
     path('profile/<int:pk>/update', ProfileUpdate.as_view(), name='profileupdate'),
     path('profile/<int:pk>', ProfileDetail.as_view(), name='profileDetail'),
-    # path('signUp/complete', signUp_complete, name = 'signUp_complete'),
 ]
